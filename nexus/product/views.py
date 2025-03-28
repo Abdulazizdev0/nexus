2f3a522b944224649be5f877b57948b2ee4c594c
Abdulazizdev0 <abdulazizmuhammadyunusov9@gmail.com>
2025-03-28 18:37:29 +0500

    add product with user

--- nexus/product/views.py
+++ nexus/product/views.py
@@ -3,6 +3,8 @@ from django.db.models import Prefetch
 from .models import *
 from hitcount.views import HitCountDetailView, HitCountMixin
 from hitcount.utils import get_hitcount_model
+# from django.core.paginator import Paginator
+from user.views import logout_view
 
 
 
@@ -11,11 +13,16 @@ def product_list(request,):
     regions = Region.objects.all()
     products = Product.objects.prefetch_related(Prefetch('images',
             queryset=ProductImage.objects.filter(is_main=True),to_attr='main_images'))
+    # p = Paginator(products,4)
+    # page = request.GET.get('page')
+    # _product = p.get_page(page)
+    # nums = "s"*product_list.paginator.num_pages
     ctx = {
         "categories":categories,
         "regions":regions,
-        "products":products
-
+        "products":products,
+        # "_product":_product,
+        # "nums":nums
 
     }
     return render(request,'products.html',ctx)
@@ -35,3 +42,18 @@ def product_detail(request,pk):
 
     }
     return render(request,'detail.html',ctx)
+
+def product_add(request):
+    categories = Category.objects.filter(is_main=True)
+    regions = Region.objects.all()
+
+    ctx={
+        "categories":categories,
+        "regions":regions,
+
+    }
+    return render(request,'product_add.html',ctx)
+
+
+
+
